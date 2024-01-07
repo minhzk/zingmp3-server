dabd9abbe3e6e47cf58b9a97c15c644b46c6bb34
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2024-01-07 09:50:35 +0700

    feat: full api

---
+++ index.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const express = require("express")
+const app = express()
+const cors = require("cors")
+require('dotenv').config()
+const port = process.env.PORT || 3012
+
+// Page Home
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+// ZingMp3Router
+const ZingMp3Router = require("./routers/api/ZingRouter")
+app.use("/api", cors({ origin: process.env.URL_CLIENT}), ZingMp3Router)
+
+// Page Error
+app.get("*", (req, res) => {
+  res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
+});
+
+app.listen(port, () => {
+  console.log(`Start server-zingmp3 listen at http://localhost:${port}`)
+});
